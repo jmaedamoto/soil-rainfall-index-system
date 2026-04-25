e111cb1f31afb8f5ad61468e5df2a6fb805df455
jmaedamoto <100673024+jmaedamoto@users.noreply.github.com>
2026-04-25 03:21:30 +0000

    Extend 24-hour rainfall adjustment tests

--- server/test_rainfall_adjustment_ratio.py
+++ server/test_rainfall_adjustment_ratio.py
@@ -185,3 +185,128 @@ def test_build_filled_mesh_rainfall_from_session_uses_max_value_for_shared_mesh_
     adjusted = service.build_filled_mesh_rainfall_from_session(prefectures, adjustments)
 
     assert adjusted["shared-mesh"] == [(3, 20.0)]
+
+
+def test_aggregate_rainfall_24hour_from_session_uses_max_mesh_sum():
+    service = RainfallAdjustmentService()
+
+    prefectures = {
+        "28": {
+            "name": "兵庫県",
+            "areas": [
+                {
+                    "name": "神戸市",
+                    "meshes": [
+                        {
+                            "code": "mesh-1",
+                            "rain_timeline": [{"ft": ft, "value": 1.0} for ft in (3, 6, 9, 12, 15, 18, 21, 24)],
+                        },
+                        {
+                            "code": "mesh-2",
+                            "rain_timeline": [{"ft": ft, "value": 2.0} for ft in (3, 6, 9, 12, 15, 18, 21, 24)],
+                        },
+                    ],
+                }
+            ],
+            "secondary_subdivisions": [{"name": "阪神", "area_names": ["神戸市"]}],
+        }
+    }
+
+    area, subdiv = service.aggregate_rainfall_24hour_from_session(prefectures)
+
+    assert area["兵庫県_神戸市"] == [{"ft": 24, "value": 16}, {"ft": 48, "value": 0}]
+    assert subdiv["兵庫県_阪神"] == [{"ft": 24, "value": 16}, {"ft": 48, "value": 0}]
+
+
+def test_build_fill_24hour_uniform_from_session_distributes_evenly():
+    service = RainfallAdjustmentService()
+
+    prefectures = {
+        "28": {
+            "name": "兵庫県",
+            "areas": [
+                {
+                    "name": "神戸市",
+                    "meshes": [
+                        {
+                            "code": "mesh-1",
+                            "rain_timeline": [{"ft": ft, "value": 1.0} for ft in (3, 6, 9, 12, 15, 18, 21, 24)],
+                        }
+                    ],
+                }
+            ],
+        }
+    }
+
+    adjusted = service.build_fill_24hour_uniform_from_session(
+        prefectures,
+        {"兵庫県_神戸市": {24: 80.0}},
+    )
+
+    assert adjusted["mesh-1"] == [(3, 10.0), (6, 10.0), (9, 10.0), (12, 10.0), (15, 10.0), (18, 10.0), (21, 10.0), (24, 10.0)]
+
+
+def test_build_ratio_24hour_uniform_from_session_scales_each_ft_from_equal_distribution():
+    service = RainfallAdjustmentService()
+
+    prefectures = {
+        "28": {
+            "name": "兵庫県",
+            "areas": [
+                {
+                    "name": "神戸市",
+                    "meshes": [
+                        {
+                            "code": "mesh-1",
+                            "rain_timeline": [{"ft": ft, "value": 1.0} for ft in (3, 6, 9, 12, 15, 18, 21, 24)],
+                        },
+                        {
+                            "code": "mesh-2",
+                            "rain_timeline": [{"ft": ft, "value": 2.0} for ft in (3, 6, 9, 12, 15, 18, 21, 24)],
+                        },
+                    ],
+                }
+            ],
+        }
+    }
+
+    adjusted = service.build_ratio_24hour_uniform_from_session(
+        prefectures,
+        {"兵庫県_神戸市": {24: 80.0}},
+    )
+
+    assert adjusted["mesh-1"][0] == (3, 5.0)
+    assert adjusted["mesh-2"][0] == (3, 10.0)
+
+
+def test_build_ratio_24hour_peak_mesh_from_session_preserves_mesh_24hour_ratios():
+    service = RainfallAdjustmentService()
+
+    prefectures = {
+        "28": {
+            "name": "兵庫県",
+            "areas": [
+                {
+                    "name": "神戸市",
+                    "meshes": [
+                        {
+                            "code": "mesh-1",
+                            "rain_timeline": [{"ft": 3, "value": 1.0}, {"ft": 6, "value": 3.0}],
+                        },
+                        {
+                            "code": "mesh-2",
+                            "rain_timeline": [{"ft": 3, "value": 2.0}, {"ft": 6, "value": 6.0}],
+                        },
+                    ],
+                }
+            ],
+        }
+    }
+
+    adjusted = service.build_ratio_24hour_peak_mesh_from_session(
+        prefectures,
+        {"兵庫県_神戸市": {24: 16.0}},
+    )
+
+    assert adjusted["mesh-2"] == [(3, 4.0), (6, 12.0)]
+    assert adjusted["mesh-1"] == [(3, 2.0), (6, 6.0)]
--- server/test_session_adjustment_mode.py
+++ server/test_session_adjustment_mode.py
@@ -12,7 +12,7 @@ sys.path.append(str(PROJECT_ROOT / "src"))
 from src.services.session_service import SessionService
 
 
-def test_fork_session_keeps_adjustment_mode():
+def test_fork_session_keeps_adjustment_mode_and_input_mode():
     service = SessionService()
 
     prefectures = {
@@ -36,11 +36,14 @@ def test_fork_session_keeps_adjustment_mode():
         base_session_id=base_session_id,
         adjustments={"兵庫県_神戸市": [{"ft": 3, "value": 10}]},
         recalculated_meshes={},
+        input_mode="24hour",
         adjustment_mode="fill_3hour",
     )
 
     session = service.get_session(fork_session_id)
     info = service.get_session_info(fork_session_id)
 
+    assert session["input_mode"] == "24hour"
     assert session["adjustment_mode"] == "fill_3hour"
+    assert info["input_mode"] == "24hour"
     assert info["adjustment_mode"] == "fill_3hour"
